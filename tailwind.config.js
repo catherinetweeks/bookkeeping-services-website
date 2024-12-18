903b8cc56f6015001c43222f0b29be76df3375d3
Catherine Weeks <catherinetweeks@gmail.com>
2024-12-18 17:20:52 -0500

    added main colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'navy' : '#152942',
+      'teal' : '#284B63',
+      'grey' : '#EEF0EB',
+      'coral' : '#FA9173'
+    },
     extend: {},
   },
   plugins: [],
